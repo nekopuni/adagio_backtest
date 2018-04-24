1d453ca72a551f38e03346074650593a1f710d4f
thoriuchi0531 <nekopuni@gmail.com>
2018-04-24 05:31:11 +0100

    Use exponentially weighing for trend-following benchmark

--- adagio_backtest/run_trend_following_benchmark.py
+++ adagio_backtest/run_trend_following_benchmark.py
@@ -11,39 +11,53 @@ if __name__ == '__main__':
 
     # common parameters
     lo_vol_scale_params = {
-        keys.vs_window: 63,
         keys.vs_chg_rule: '+Wed-1bd+1bd',
         keys.vs_target_vol: 0.1,
+        keys.vs_method_params: {
+            keys.vs_method: keys.vs_ewm,
+            keys.vs_ewm_halflife: 21,
+        }
     }
     momentum_params = {
-        keys.signal_type: keys.momentum,
-        keys.signal_windows: [[8, 24], [16, 48], [32, 96]],
+        keys.signal_method_params: {
+            keys.signal_method: keys.signal_trend_ma_xover,
+            keys.signal_windows: [[8, 24], [16, 48], [32, 96]],
+        },
         keys.signal_chg_rule: '+Wed-1bd+1bd',
         keys.signal_to_position: keys.linear,
         keys.position_cap: 1.0,
         keys.position_floor: -1.0
     }
     portfolio_params = {
-        keys.weighting: keys.equal_weight
+        keys.weighting: keys.equal_weight,
+        keys.port_weight_chg_rule: '+Wed-1bd+1bd',
     }
     port_vol_scale_params = {
-        keys.vs_window: 252,
         keys.vs_chg_rule: '+Wed-1bd+1bd',
         keys.vs_target_vol: 0.1,
+        keys.vs_method_params: {
+            keys.vs_method: keys.vs_ewm,
+            keys.vs_ewm_halflife: 63,
+        }
     }
 
     # parameters by asset-class
-    equity_lo_params = {
-        keys.lo_ticker: ['CME_SP', 'CME_NQ', 'CME_MD', 'ICE_RF',
+    equity_dm_lo_params = {
+        keys.lo_ticker: ['CME_ES', 'CME_NQ', 'CME_YM', 'CME_MD', 'ICE_RF',
                          'CME_NK', 'EUREX_FESX', 'EUREX_FDAX',
-                         'EUREX_FSMI', 'LIFFE_FCE', 'LIFFE_Z', 'LIFFE_FTI'],
+                         'EUREX_FSMI', 'LIFFE_FCE', 'LIFFE_Z', 'LIFFE_FTI',
+                         'CFFEX_IF', 'MX_SXF'],
+        keys.slippage: 2.0,
+    }
+    equity_em_lo_params = {
+        keys.lo_ticker: ['SGX_IN', 'LIFFE_PSI', 'SGX_ID', 'SGX_SG', 'SGX_TW'],
         keys.slippage: 2.0,
     }
     rates_lo_params = {
         keys.lo_ticker: ['CME_TU', 'CME_FV', 'CME_TY', 'CME_US', 'CME_UL',
                          'EUREX_FGBS', 'EUREX_FGBM', 'EUREX_FGBL', 'EUREX_FGBX',
-                         'EUREX_FBTP', 'EUREX_FOAT', 'MX_CGB', 'LIFFE_R',
-                         'SGX_JB'],
+                         'EUREX_FBTS', 'EUREX_FBTP', 'EUREX_FOAT', 'MX_CGB',
+                         'LIFFE_R', 'SGX_JB'],
         keys.slippage: 2.0,
     }
     fx_dm_lo_params = {
@@ -52,21 +66,22 @@ if __name__ == '__main__':
         keys.slippage: 2.0,
     }
     fx_em_lo_params = {
-        keys.lo_ticker: ['CME_MP', 'CME_RU', 'CME_BR', 'CME_RA', 'CME_PZ',
-                         'CME_TRY', 'CME_CNH', 'DGCX_DINR'],
+        keys.lo_ticker: ['CME_MP', 'CME_RU', 'CME_BR', 'CME_RA'],
         keys.slippage: 2.0,
     }
     comdty_energy_lo_params = {
         keys.lo_ticker: ['CME_CL', 'CME_HO', 'CME_RB', 'CME_NG', 'ICE_B',
-                         'ICE_G'],
+                         'ICE_G', 'ICE_M'],
         keys.slippage: 2.0,
     }
     comdty_grains_lo_params = {
-        keys.lo_ticker: ['CME_C', 'CME_W', 'CME_S', 'CME_KW'],
+        keys.lo_ticker: ['CME_C', 'CME_W', 'CME_S', 'CME_KW', 'ICE_RS',
+                         'LIFFE_EBM', 'LIFFE_ECO', 'MGEX_MW'],
         keys.slippage: 2.0,
     }
     comdty_softs_lo_params = {
-        keys.lo_ticker: ['ICE_SB', 'ICE_KC', 'ICE_CT', 'ICE_CC'],
+        keys.lo_ticker: ['ICE_SB', 'ICE_KC', 'ICE_CT', 'ICE_CC', 'ICE_OJ',
+                         'LIFFE_W'],
         keys.slippage: 2.0,
     }
     comdty_agri_lo_params = {
@@ -79,18 +94,30 @@ if __name__ == '__main__':
         keys.slippage: 2.0,
     }
 
-    # Equity
-    engine_equity = adagio.Engine(name='equity_momentum')
-    engine_equity.add(adagio.LongOnly(**equity_lo_params))
-    engine_equity.add(adagio.VolatilityScaling(name='vs_pre_signal',
-                                               **lo_vol_scale_params))
-    engine_equity.add(adagio.Signal(**momentum_params))
-    engine_equity.add(adagio.Portfolio(name='equity_port', **portfolio_params))
-    engine_equity.add(adagio.PortVolatilityScaling(name='equity_port_vs',
-                                                   **port_vol_scale_params))
+    # Equity Developed markets
+    engine_equity_dm = adagio.Engine(name='Equity DM Trend-following')
+    engine_equity_dm.add(adagio.LongOnly(**equity_dm_lo_params))
+    engine_equity_dm.add(adagio.VolatilityScaling(name='vs_pre_signal',
+                                                  **lo_vol_scale_params))
+    engine_equity_dm.add(adagio.Signal(**momentum_params))
+    engine_equity_dm.add(adagio.Portfolio(name='equity_port',
+                                          **portfolio_params))
+    engine_equity_dm.add(adagio.PortVolatilityScaling(name='equity_port_vs',
+                                                      **port_vol_scale_params))
+
+    # Equity Emerging markets
+    engine_equity_em = adagio.Engine(name='Equity EM Trend-following')
+    engine_equity_em.add(adagio.LongOnly(**equity_em_lo_params))
+    engine_equity_em.add(adagio.VolatilityScaling(name='vs_pre_signal',
+                                                  **lo_vol_scale_params))
+    engine_equity_em.add(adagio.Signal(**momentum_params))
+    engine_equity_em.add(adagio.Portfolio(name='equity_port',
+                                          **portfolio_params))
+    engine_equity_em.add(adagio.PortVolatilityScaling(name='equity_port_vs',
+                                                      **port_vol_scale_params))
 
     # Rates
-    engine_rates = adagio.Engine(name='rates_momentum')
+    engine_rates = adagio.Engine(name='Rates Trend-following')
     engine_rates.add(adagio.LongOnly(**rates_lo_params))
     engine_rates.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                               **lo_vol_scale_params))
@@ -100,7 +127,7 @@ if __name__ == '__main__':
                                                   **port_vol_scale_params))
 
     # FX Developed markets
-    engine_fx_dm = adagio.Engine(name='fx_dm_momentum')
+    engine_fx_dm = adagio.Engine(name='FX DM Trend-following')
     engine_fx_dm.add(adagio.LongOnly(**fx_dm_lo_params))
     engine_fx_dm.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                               **lo_vol_scale_params))
@@ -110,7 +137,7 @@ if __name__ == '__main__':
                                                   **port_vol_scale_params))
 
     # FX Emerging markets
-    engine_fx_em = adagio.Engine(name='fx_em_momentum')
+    engine_fx_em = adagio.Engine(name='FX EM Trend-following')
     engine_fx_em.add(adagio.LongOnly(**fx_em_lo_params))
     engine_fx_em.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                               **lo_vol_scale_params))
@@ -120,7 +147,7 @@ if __name__ == '__main__':
                                                   **port_vol_scale_params))
 
     # Commodity energy futures
-    engine_energy = adagio.Engine(name='energy_momentum')
+    engine_energy = adagio.Engine(name='Energy Trend-following')
     engine_energy.add(adagio.LongOnly(**comdty_energy_lo_params))
     engine_energy.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                                **lo_vol_scale_params))
@@ -130,7 +157,7 @@ if __name__ == '__main__':
                                                    **port_vol_scale_params))
 
     # Commodity grain futures
-    engine_grains = adagio.Engine(name='grain_momentum')
+    engine_grains = adagio.Engine(name='Grain Trend-following')
     engine_grains.add(adagio.LongOnly(**comdty_grains_lo_params))
     engine_grains.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                                **lo_vol_scale_params))
@@ -140,7 +167,7 @@ if __name__ == '__main__':
                                                    **port_vol_scale_params))
 
     # Commodity soft futures
-    engine_soft = adagio.Engine(name='soft_momentum')
+    engine_soft = adagio.Engine(name='Soft Trend-following')
     engine_soft.add(adagio.LongOnly(**comdty_softs_lo_params))
     engine_soft.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                              **lo_vol_scale_params))
@@ -150,7 +177,7 @@ if __name__ == '__main__':
                                                  **port_vol_scale_params))
 
     # Commodity agriculture futures
-    engine_agri = adagio.Engine(name='agri_momentum')
+    engine_agri = adagio.Engine(name='Agri Trend-following')
     engine_agri.add(adagio.LongOnly(**comdty_agri_lo_params))
     engine_agri.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                              **lo_vol_scale_params))
@@ -160,7 +187,7 @@ if __name__ == '__main__':
                                                  **port_vol_scale_params))
 
     # Commodity metal futures
-    engine_metal = adagio.Engine(name='metal_momentum')
+    engine_metal = adagio.Engine(name='Metal Trend-following')
     engine_metal.add(adagio.LongOnly(**comdty_metal_lo_params))
     engine_metal.add(adagio.VolatilityScaling(name='vs_pre_signal',
                                               **lo_vol_scale_params))
@@ -170,13 +197,23 @@ if __name__ == '__main__':
                                                   **port_vol_scale_params))
 
     # Momentum strategy basket
-    engine_momentum = adagio.Engine(name='engine_momentum')
-    engine_momentum.add([engine_equity, engine_rates, engine_fx_dm,
-                         engine_fx_em, engine_energy, engine_grains,
+    engine_momentum = adagio.Engine(name='Trend-following benchmark')
+    engine_momentum.add([engine_equity_dm, engine_equity_em,
+                         engine_rates, engine_fx_dm, engine_fx_em,
+                         engine_energy, engine_grains,
                          engine_soft, engine_agri, engine_metal])
     engine_momentum.add(
-        adagio.Portfolio(weighting=[1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6,
-                                    1 / 6 / 4, 1 / 6 / 4, 1 / 6 / 4, 1 / 6 / 4])
+        adagio.Portfolio(weighting=[1 / 4 * 0.9,  # equity dm
+                                    1 / 4 * 0.1,  # equity em
+                                    1 / 4,  # rates
+                                    1 / 4 * 2 / 3,  # fx dm
+                                    1 / 4 * 1 / 3,  # fx em
+                                    1 / 4 * 0.2,  # energy
+                                    1 / 4 * 0.2,  # grains
+                                    1 / 4 * 0.2,  # soft
+                                    1 / 4 * 0.2,  # agri
+                                    1 / 4 * 0.2  # metal
+                                    ])
     )
     engine_momentum.add(adagio.PortVolatilityScaling(**port_vol_scale_params))
     engine_momentum.backtest()
